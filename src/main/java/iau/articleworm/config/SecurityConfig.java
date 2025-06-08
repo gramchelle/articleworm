c0e3c14aa587e753965c57e21938c081d32fdf7e
Özlem Nur Duman <ozlemnduman@stu.aydin.edu.tr>
2025-06-08 23:07:08 +0300

    web config is added

--- src/main/java/iau/articleworm/config/SecurityConfig.java
+++ src/main/java/iau/articleworm/config/SecurityConfig.java
@@ -15,7 +15,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import iau.articleworm.service.concrete.CustomUserDetailsService;
-import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
@@ -41,7 +40,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
             .authorizeHttpRequests()
-            .requestMatchers("/api/auth/**").permitAll()
+            .requestMatchers("/api/auth/login").permitAll()
+            .requestMatchers("/api/auth/register").permitAll()
             .requestMatchers("/admin/**").hasAuthority("ADMIN")
             .requestMatchers("/author/**").hasAuthority("AUTHOR")
             .requestMatchers("/reader/**").hasAnyAuthority("READER", "AUTHOR", "ADMIN")
