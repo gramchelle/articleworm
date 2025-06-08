4206114e0e16e494582dae78c2b2410df196a2e3
Özlem Nur Duman <ozlemnduman@stu.aydin.edu.tr>
2025-06-08 23:47:39 +0300

    securityconfig is modified to permit all requests

--- src/main/java/iau/articleworm/config/SecurityConfig.java
+++ src/main/java/iau/articleworm/config/SecurityConfig.java
@@ -38,20 +38,10 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
+        http
+            .csrf().disable()
             .authorizeHttpRequests()
-            .requestMatchers("/api/auth/login").permitAll()
-            .requestMatchers("/api/auth/register").permitAll()
-            .requestMatchers("/admin/**").hasAuthority("ADMIN")
-            .requestMatchers("/author/**").hasAuthority("AUTHOR")
-            .requestMatchers("/reader/**").hasAnyAuthority("READER", "AUTHOR", "ADMIN")
-            .anyRequest().authenticated()
-            .and()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
-            .authenticationProvider(authenticationProvider())
-            .addFilterBefore(jwtAuthFilter(), UsernamePasswordAuthenticationFilter.class);
-
+                .anyRequest().permitAll();
         return http.build();
     }
 
